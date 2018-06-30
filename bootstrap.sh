8c23004a38f65be4f1fb1e93b38886d4e0ee294f
Richard Bishop <richard@rubiquity.com>
2018-06-30 16:20:54 -0700

    zsh is the shell used now

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ function doIt() {
   cd ~/.vim/bundle
 	while read -r line; do git clone $line; done < vim-bundles.txt;
   rm vim-bundles.txt
-	source ~/.bash_profile
+	source ~/.zprofile
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
