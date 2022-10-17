0c621b4a51e27a01f73df9d0aa249862d119327b
Richard Bishop <richard.bishop@crunchydata.com>
2022-10-17 10:49:28 -0700

    remove nix file pull script
    nixos configs source is in the repo now

--- pull-nix-files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp /etc/nixos/configuration.nix ./nixos/configuration.nix
-cp /etc/nixos/hardware-configuration.nix ./nixos/hardware-configuration.nix
-cp ~/.config/nixpkgs/home.nix ./nixos/home.nix
