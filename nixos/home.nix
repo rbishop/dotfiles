d46903b35d1c75a006aadcff7ff772c1bb1bf3db
Richard Bishop <richard@rubiquity.com>
2022-12-27 12:56:04 -0800

    easier pane/window navigation in tmux

--- nixos/home.nix
+++ nixos/home.nix
@@ -146,6 +146,38 @@ in
     resizeAmount = 10;
     prefix = "C-Space";
     terminal = "xterm-256color";
+    extraConfig = ''
+      # browser style tab navigation
+      bind -n M-1 select-window -t 1
+      bind -n M-2 select-window -t 2
+      bind -n M-3 select-window -t 3
+      bind -n M-4 select-window -t 4
+      bind -n M-5 select-window -t 5
+      bind -n M-6 select-window -t 6
+      bind -n M-7 select-window -t 7
+      bind -n M-8 select-window -t 8
+      bind -n M-9 select-window -t:$
+
+      # left and right window navigation
+      bind -n M-Left select-window -t:-1
+      bind -n M-Right select-window -t:+1
+
+      # vim style pane navigation
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # split windows into panes like vim
+      bind s split-window -v
+      bind v split-pane -h
+
+      set -g status-style "bg=olive,fg=black"
+      set -g window-status-current-style "bg=olive,fg=black,reverse,bold"
+
+      set -g status-left ""
+      set -g status-right ""
+    '';
   };
 
   programs.git = {
@@ -162,7 +194,6 @@ in
       vo = "gpu";
       hwdec = "vaapi";
       glsl-shader = "~~/shaders/FSRCNNX_x2_16-0-4-1.glsl";
-      #glsl-shader = "~~/shaders/FSRCNNX_x2_8-0-4-1.glsl";
       ao = "alsa";
       audio-device = "auto";
     };
