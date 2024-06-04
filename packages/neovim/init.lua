44658baa8962de4edea4f6db79168b8363f60ea5
Richard Bishop <richard.bishop@crunchydata.com>
2024-06-04 16:31:39 -0700

    nvim: remove highlights after search/replace

--- packages/neovim/init.lua
+++ packages/neovim/init.lua
@@ -13,7 +13,6 @@ require("ruby")
 
 vim.cmd([[
   set termguicolors
-
   syntax on
   filetype plugin indent on
   set t_Co=256
@@ -22,18 +21,16 @@ vim.cmd([[
   set autowriteall
   set splitright
   :au FocusLost * :wa
-
   set exrc
-
   set cursorline
   hi CursorLine cterm=NONE ctermbg=235
   set cursorcolumn
   hi CursorColumn cterm=NONE ctermbg=235
   nnoremap <Leader>c :set cursorline! cursorcolumn!<CR>
+  nnoremap <Leader>/ :nohlsearch<CR>
   command! Files lua require("fzf-commands").files()
   nmap <Leader>f :Files<CR>
   map - :Ex<CR>
-
   autocmd Filetype go setlocal ts=4 sts=4 sw=4
   autocmd Filetype rb setlocal ts=2 sts=2 sw=2
   autocmd InsertLeave * if expand('%') != "" | update | endif
